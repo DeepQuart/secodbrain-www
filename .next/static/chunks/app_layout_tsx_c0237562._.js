a92ea15a4da7fe081aec22f66c18bfa02e93d16b
tejasteju12 <steja.1si21ad059@gmail.com>
2025-06-11 20:41:03 +0530

    fix: update next headers chunk references for consistency

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++ .next/static/chunks/app_layout_tsx_c0237562._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_next_headers_3a84af82.js",
+  "static/chunks/node_modules_next_headers_07d90581.js",
   "static/chunks/node_modules_framer-motion_dist_es_24e93cb0._.js",
   "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
   "static/chunks/node_modules_better-auth_dist_f1c6d9ca._.js",
@@ -9,8 +9,8 @@
   "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
   "static/chunks/node_modules_jose_dist_browser_c98a78c6._.js",
   "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
-  "static/chunks/node_modules_3537a1c6._.js",
-  "static/chunks/_44683a0e._.js",
+  "static/chunks/node_modules_1ff4b8f2._.js",
+  "static/chunks/_5937767e._.js",
   "static/chunks/app_globals_73c37791.css"
 ],
     source: "dynamic"
